61241996535756c9f88512bd702b1c6366ebde48
Tomasz Jaszczynski <69143277+tomasz-pexip@users.noreply.github.com>
2024-02-16 11:02:51 +0000

    Update Pexip_Log_Tools.ps1
    improved decryption if video files

--- Pexip_Log_Tools.ps1
+++ Pexip_Log_Tools.ps1
@@ -784,7 +784,7 @@ function Unprotect-PexFile {
                     # Use the GNU tool 'file.exe' to determine if the file is readable as a non 'data' file.
                     # If not, then decryption probably failed so restore the file.
                     # PCAPNG and PCAP files (Wireshark), will still read as data files, so ignore checking those.
-                    if (((& $PathToFile $FileItem | Select-String -pattern "; data" -quiet) -eq $true) -and ($FileItem.Extension -notin ".pcap", ".pcapng"))
+                    if (((& $PathToFile $FileItem | Select-String -pattern "; data" -quiet) -eq $true) -and ($FileItem.Extension -notin ".pcap", ".pcapng", ".webm", ".mp4", ".avi"))
                     {
                         Write-Warning "The file did not decrypt properly, the key may be wrong."
                         Write-Warning "Restoring original file and exiting."
